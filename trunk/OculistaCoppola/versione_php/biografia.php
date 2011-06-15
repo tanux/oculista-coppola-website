45eee7486277d5dc223e55c6888301c07f6f6dcc
espositogaetano87 <espositogaetano87@c94e9afb-5d99-2f88-7c2c-a80bc0848855>
2011-06-15 14:48:50 +0000

    ottimizzazione immagini slider + applicazione tecnica css-sprite per titoli pagina

--- trunk/OculistaCoppola/versione_php/biografia.php
+++ trunk/OculistaCoppola/versione_php/biografia.php
@@ -7,9 +7,7 @@
         <meta name="description" content="Biografia del Dott.Salvatore Coppola, oculista di avellino" />
 <?php   require_once 'init.php'; ?>
     <div id="contenuto">
-        <div id="titolo_pagina_corrente">
-            <img src="img/biografia.png" />
-        </div>
+        <div class="titolo_pagina_corrente" style="background-image:url('img/titoli_pagina.gif'); background-position:0px -37px"></div>
         <div id="testo_pagina_corrente" style="width:860px">
             <table>
                 <tr>
--- trunk/OculistaCoppola/versione_php/sitemap.php
+++ trunk/OculistaCoppola/versione_php/sitemap.php
@@ -16,14 +16,29 @@ $sxml = simplexml_load_file($feedURL);
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
+        <style type="text/css">
+          ul{
+            list-style-image:url('img/bullet.gif');
+          }
+          li{
+            line-height:30px;
+          }
+        </style>
         <title>Home Page | Dott.Salvatore Coppola - Oculista</title>
         <meta name="description" content="Home page del sito del dottor salvatore coppola, oculista di avellino" />
 <?php   require_once 'init.php'; ?>
         <div id="contenuto">
-            <div id="titolo_pagina_corrente">
-                <img src="img/home_page.png" />
-            </div>
+            <div class="titolo_pagina_corrente" style="background-image:url('img/titoli_pagina.gif'); background-position:0px -289px;"></div>
             <div id="testo_pagina_corrente" style="width:560px;">
+              <ul>
+                <li><a href="index.php">Home Page</a></li>
+                <li><a href="biografia.php">Biografia</a></li>
+                <li><a href="immagini.php">Immagini</a></li>
+                <li><a href="video.php">Video</a></li>
+                <li><a href="contatti.php">Contatti</a></li>
+                <li><a href="dove_sono.php">Dove Sono</a></li>
+                <li><a href="note_legali.php">Note Legali</a></li>
+              </ul>
             </div>
             <div id="sc_image"></div>
         </div>
