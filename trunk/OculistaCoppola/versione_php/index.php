224e1813395f2e93a506a6273317a83fd3f8961c
espositogaetano87 <espositogaetano87@c94e9afb-5d99-2f88-7c2c-a80bc0848855>
2011-06-16 15:24:21 +0000

    modificata struttura homepage

--- trunk/OculistaCoppola/versione_php/index.php
+++ trunk/OculistaCoppola/versione_php/index.php
@@ -16,59 +16,76 @@ $sxml = simplexml_load_file($feedURL);
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
+        <style type="text/css">
+          ul{
+            padding-left:20px;
+            line-height:30px;
+            list-style-image:url('img/bullet.gif');
+          }
+          li{
+            font-family:verdana;
+            font-size:12px;
+          }
+        </style>
         <title>Home Page | Dott.Salvatore Coppola - Oculista</title>
         <meta name="description" content="Home page del sito del dottor salvatore coppola, oculista di avellino" />
 <?php   require_once 'init.php'; ?>
         <div id="contenuto">
             <div class="titolo_pagina_corrente" style="background-image:url('img/titoli_pagina.gif')"></div>
-            <div id="testo_pagina_corrente" style="width:560px;">
-                L'oculistica (o oftalmologia, oftalmoiatria) &egrave; la branca della medicina che si occupa di prevenzione, diagnosi e terapia delle malattie dell'occhio, della misurazione della vista e della correzione dei vizi refrattivi. Si tratta di una delle discipline mediche pi&ugrave; antiche. Già nel Codice di Hammurabi più di 3600 anni fa furono promulgate regolamentazioni per operazioni agli occhi - il medico doveva ottenere una retribuzione di 10 scicli per un'operazione ad esito positivo, mentre in caso di esito negativo gli sarebbero state tagliate le mani. All'epoca ellenistica risalgono sia i primi studi accurati dell'anatomia dell'occhio (in particolare ad opera di Erofilo da Calcedonia, secondo quanto raccontato da Aezio di Amida)[senza fonte] che le prime operazioni di rimozioni della cataratta. Nel Medioevo il compito dell'oculista venne assunto dal cosiddetto "incisore della cataratta". Grazie ad un coltello speciale il cristallino opaco ("cataratta") veniva premuto dentro all'occhio. Johann Sebastian Bach morì in seguito ad un'operazione alla cataratta. Georg Friedrich Händel sopravvisse alla sua prima operazione, ma rimase cieco per tutta la sua vita.
-                L'oculistica apparteneva inizialmente alla chirurgia e si è sviluppata solo nel corso del XVIII secolo, in particolare tuttavia nel XIX secolo come materia autonoma. Fino al XVIII secolo l'anatomia e la modalità di funzione dell'occhio era poco chiara. Solo a partire dal XIX secolo si scoprirono particolari grazie al frequente utilizzo del microscopio e sistematicamente utilizzabili per la terapia. nel 1800 Carl Gustav Himly coniò il termine oftalmologia, nello stesso anno Thomas Young descrisse l'astigmatismo.
-                Le prime cliniche furono aperte all'inizio del XIX secolo a Erfurt e Budapest. La prima cattedra di oftalmologia fu occupata da Georg Joseph Beer (1763-1821) che nel 1818 era diventato ordinario di oculistica a Vienna. Precedentemente vi aveva aperto nel 1813 la prima clinica universitaria per malati agli occhi.
+            <div id="testo_pagina_corrente" style="width:395px; float:left; font-size:15px; padding-bottom:55px">
+              oculistacoppola.it nasce dall’idea del Dott. Coppola Salvatore  di condividere le sue
+              esperienze e far conoscere il proprio lavoro su larga scala, attraverso l’utilizzo del web.
+              <br />
+              Le sezioni immagini e video (in costante aggiornamento) mostrano i risultati maturati negli
+              anni con l’accrescere dell’esperienza nel campo dell’oculistica.
+              <br />
+              Attualmente esercita le proprie competenze presso l’A.S.L. di Avellino ed è coordinatore
+              del servizio di chirurgia oculare presso la struttura ospedaliera di Sant’Angelo dei Lombardi.
+            </div>
+            <div id="thumb_coppola" style="width:474px; height:329px; margin-left:405px;background-image:url('img/thumb_home.png')"></div>
+            <div id="content_box_news">
+              <div id="box_news_immagini" class="box_news">
+                  <div class="titolo_box_news">Nuove Immagini</div>
+                  <div class="news"style="position: relative; margin-left: 10px; width: 248px; margin-top: 45px; height: 120px;">
+                    <ul>
+                      <?php
+                        for($i=0; $i<4; $i++){
+                          $id = $photosets['photoset'][$i]['id'];
+                          $nome_album = $photosets['photoset'][$i]['title'];
+                      ?>
+                          <li><strong><a href="immagini.php#<?php echo $id?>"><?php echo $nome_album?></a></strong>
+                      <?php
+                        }
+                      ?>
+                    </ul>
+                  </div>
+              </div>
+              <div id="box_news_video" class="box_news">
+                  <div class="titolo_box_news">Nuovi Video</div>
+                  <div class="news" style="position: relative; margin-left: 10px; width: 248px; margin-top: 45px; height: 120px;">
+                    <ul>
+                      <?php
+                        $i=0;
+                        foreach ($sxml->entry as $entry){
+                          $media = $entry->children('http://search.yahoo.com/mrss/');
+                          $attrs = $media->group->player->attributes();
+                          $url = $attrs['url'];
+                          $video = YoutubeDataApi::getVideoResponse($url);
+                          $titolo = $video->getTitle();
+                          $thumbnails = $video->getThumbnails();
+                          $thumbnail = $thumbnails[0]['url'];
+                          $i++;
+                          if ($i>4)
+                            continue;
+                      ?>
+                          <li><strong><a href="video.php#<?php echo $url?>"><?php echo $titolo?></a></strong>
+                      <?php
+                        }
+                      ?>
+                    </ul>
+                  </div>
+              </div>
             </div>
             <div id="sc_image"></div>
-        </div>
-        <div id="content_box_news">
-            <div id="box_news_immagini" class="box_news">
-                <div class="titolo_box_news">Nuove Immagini</div>
-                <div class="news"style="position: relative; margin-left: 10px; width: 248px; margin-top: 45px; height: 120px;">
-                  <ul style="padding-left:20px; line-height:30px">
-                    <?php
-                      for($i=0; $i<4; $i++){
-                        $id = $photosets['photoset'][$i]['id'];
-                        $nome_album = $photosets['photoset'][$i]['title'];
-                    ?>
-                        <li type="circle" style="font-family:verdana; font-size:12px">Nuovo album:<strong><a href="immagini.php#<?php echo $id?>"><?php echo $nome_album?></a></strong>
-                    <?php
-                      }
-                    ?>
-                  </ul>
-                </div>
-            </div>
-            <div id="box_news_video" class="box_news">
-                <div class="titolo_box_news">Nuovi Video</div>
-                <div class="news"style="position: relative; margin-left: 10px; width: 248px; margin-top: 45px; height: 120px;">
-                  <ul style="padding-left:20px; line-height:30px">
-                    <?php
-                      $i=0;
-                      foreach ($sxml->entry as $entry){
-                        $media = $entry->children('http://search.yahoo.com/mrss/');
-                        $attrs = $media->group->player->attributes();
-                        $url = $attrs['url'];
-                        $video = YoutubeDataApi::getVideoResponse($url);
-                        $titolo = $video->getTitle();
-                        $thumbnails = $video->getThumbnails();
-                        $thumbnail = $thumbnails[0]['url'];
-                        $i++;
-                        if ($i>4)
-                          continue;
-                    ?>
-                        <li type="circle" style="font-family:verdana; font-size:12px">Nuovo video:<strong><a href="video.php#<?php echo $url?>"><?php echo $titolo?></a></strong>
-                    <?php
-                      }
-                    ?>
-                  </ul>
-                </div>
-            </div>
         </div>
 <?php  require_once 'finish.php'?>
