db2dd3721b0fdfe81f7143239e247c3c234947ef
espositogaetano87 <espositogaetano87@c94e9afb-5d99-2f88-7c2c-a80bc0848855>
2011-06-16 14:40:54 +0000

    modificata struttura homepage

--- OculistaCoppola/versione_php/biografia.php
+++ OculistaCoppola/versione_php/biografia.php
@@ -18,11 +18,36 @@
                         </div>
                     </td>
                     <td>
-                        Laureato presso il Dipartimento di Scienze Oftalmologiche dell’Università di Siena, diplomato come specialista in oculista nel 1994 con lode effettua corsi di formazione presso il servizio di patologia della mobilità (strabismo, palpebre) con pubblicazione di numerosi lavori scientifici su utilizzo delle suture regolabili (Adjustable Stutures) nella correzione chirurgica dello strabismo nell’età adulta e inerenti le patologie della palpebra. Nel 1995 frequenta uno stage in New York presso il prof Bosniak S. per perfezionare le sue conoscenze su chirurgia delle palpebre e degli annessi (in particolar modo apprende la tecnica per la blefaroplastica etnica e funzionale superiore ed inferiore).
-Nel 1997 ancora un’esperienza negli Statu Uniti alla corte del Dottor Leslie Eisner (originario del Connecticut) da cui apprende la tecnica della facoemulsificante della cataratta in anestesia topica, ovvero non invasiva e senza utilizzo di suture (sutureless). 
-<br><br>Attualmente esercita in qualità di specialista ambulatoriale in oculistica presso la ASL AV nei distretti dell’alta Irpinia, ed è coordinatore e responsabile da dieci anni del servizio di chirurgia oculare in Day Surger presso il presidio ospedaliero di Sant’Angelo dei Lombardi. Questa esperienza maturata in alta Irpinia in ambito diagnostico e terapeutico–chirurgico si è sviluppata con un trend positivo e progressivo che ha condotto ad una numerosa casistica chirurgica di circa duemila (2000) interventi effettuati.
-<br><br>Intervento________rimane sicuramente quello della cataratta senile in quanto l’età media della popolazione irpina risulta avanzata e in quanto la tipologia della tecnica chirurgica utilizzata, poco invasiva e senza ricovero è risultata molto apprezzata ed altrettanto richiesta. Dal 2000 pratica inoltre interventi per la correzione dei difetti refrattivi mediante laser ad eccimeri: PRK, Lasik, ICL. Ove utilizza il microcheratomo amadeus  ed il laser allegretto a 400Hz.
-
+                      Laureato presso il Dipartimento di Scienze Oftalmologiche dell’Università
+                      di Siena, diplomato come specialista in Oculista nel 1994 con lode, effettua
+                      corsi di formazione presso il servizio di patologia della mobilità
+                      (<em>strabismo, palpebre</em>) con pubblicazione di numerosi lavori scientifici su
+                      utilizzo delle suture regolabili (<em>Adjustable Sutures</em>) nella correzione
+                      chirurgica dello strabismo nell’età adulta e inerenti le patologie della
+                      palpebra. Nel 1995 frequenta uno stage in New York presso il Prof.Bosniak S.,
+                      per perfezionare le sue conoscenze su chirurgia delle palpebre e degli annessi
+                      (in particolar modo apprende la tecnica per la blefaroplastica etnica e funzionale
+                      superiore ed inferiore).Nel 1997 ancora un’esperienza negli Stati Uniti
+                      alla corte del Dott.Leslie Eisner (originario del Connecticut) da cui
+                      apprende la tecnica della facoemulsificante della cataratta in anestesia
+                      topica, ovvero non invasiva e senza utilizzo di suture (<em>Sutureless</em>).
+                      <br /> <br />
+                      Attualmente esercita in qualità di specialista ambulatoriale in oculistica
+                      presso la A.S.L. di Avellino, nei distretti dell’alta Irpinia, e da dieci anni è
+                      coordinatore e responsabile del servizio di chirurgia oculare in Day Surger
+                      presso il presidio ospedaliero di Sant’Angelo dei Lombardi. Questa
+                      esperienza maturata in alta Irpinia in ambito diagnostico e
+                      terapeutico–chirurgico, si è sviluppata con un trend positivo e progressivo
+                      che ha condotto ad una numerosa casistica chirurgica di circa duemila (2000)
+                      interventi effettuati.
+                      <br /><br />
+                      Intervento________rimane sicuramente quello della cataratta senile, in quanto
+                      l’età media della popolazione irpina risulta avanzata e la tipologia
+                      della tecnica chirurgica utilizzata, poco invasiva e senza ricovero è
+                      risultata molto apprezzata ed altrettanto richiesta. Dal 2000 pratica
+                      inoltre interventi per la correzione dei difetti refrattivi mediante laser
+                      ad eccimeri: PRK, Lasik, ICL. Ove utilizza il microcheratomo amadeus  ed
+                      il laser allegretto a 400Hz.
                     </td>
                 </tr>
             </table>
